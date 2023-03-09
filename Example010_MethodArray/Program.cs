8449de45df8df43cc23e3d2264f7a438976dbf05
Dmitry Kuzmin <sinoptickzone@gmail.com>
2023-03-09 15:26:11 +0300

    Add Example010_MethodArray

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] array = { 1, 21, 32, 23, 43, 12, 23, 32 };
+int n = array.Length;
+
+int find = 23;
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
