f6113c7f29cf4c9a7689b7bd7f3b6739aa54debf
Dmitry Kuzmin <sinoptickzone@gmail.com>
2023-03-18 21:37:21 +0300

    Exactly added Example012_Methods

--- Example012_Methods/Program.cs
+++ Example012_Methods/Program.cs
@@ -116,4 +116,36 @@
 
 // Упорядочить массив – это как?
 
-// cd
+// int[] arr = {1, 5, 4, 3, 2, 8, 2, 1, 6, 7, 3, 1};
+
+// void PrintArray(int[] array)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         Console.Write($"{array[i]} ");
+//     }
+//     Console.WriteLine();
+// }
+
+// void SelectionSort(int[] array)
+// {
+//     for (int i = 0; i < array.Length - 1; i++)
+//     {
+//         int minPosition = i;
+//         for (int j = i + 1; j < array.Length; j++)
+//         {
+//             if (array[j] < array[minPosition])
+//             {
+//                 minPosition = j;
+//             }
+//         }
+//         int temporary = array[i];
+//         array[i] = array[minPosition];
+//         array[minPosition] = temporary;
+//     }
+// }
+
+// PrintArray(arr);
+// SelectionSort(arr);
+// PrintArray(arr);
+
