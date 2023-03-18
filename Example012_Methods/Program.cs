dd146ab74c3fd09148b7164a098da87a9ee8f98e
Dmitry Kuzmin <sinoptickzone@gmail.com>
2023-03-18 21:45:32 +0300

    Added Example012_Methods Homework

--- Example012_Methods/Program.cs
+++ Example012_Methods/Program.cs
@@ -149,3 +149,38 @@
 // SelectionSort(arr);
 // PrintArray(arr);
 
+
+// Homework Otherwise
+
+// int[] arr = {1, 5, 4, 3, 2, 8, 2, 1, 6, 7, 3, 1};
+
+// void PrintArray(int[] array)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         Console.Write($"{array[i]} ");
+//     }
+//     Console.WriteLine();
+// }
+
+// void SelectionSortRe(int[] array)
+// {
+//     for (int i = 0; i < array.Length - 1; i++)
+//     {
+//         int maxPosition = i;
+//         for (int j = i + 1; j < array.Length; j++)
+//         {
+//             if (array[j] > array[maxPosition])
+//             {
+//                 maxPosition = j;
+//             }
+//         }
+//         int temporary = array[i];
+//         array[i] = array[maxPosition];
+//         array[maxPosition] = temporary;
+//     }
+// }
+
+// PrintArray(arr);
+// SelectionSortRe(arr);
+// PrintArray(arr);cd..
